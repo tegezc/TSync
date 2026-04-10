5b3cedba75bac4d462533be5c66e0f40179561a2
parman <tegesitt@gmail.com>
2026-04-11 00:53:09 +0700

    feat: handle get hisoty data

--- lib/src/contracts/i_remote_stream.dart
+++ lib/src/contracts/i_remote_stream.dart
@@ -3,5 +3,12 @@ import '../models/record_change.dart';
 abstract class IRemoteStream {
   /// Membuka aliran data untuk ledger tertentu.
   /// Menghasilkan list of [RecordChange] setiap kali ada perubahan di server.
-  Stream<List<RecordChange>> getLedgerStream(String ledgerId);
+  Stream<List<RecordChange>> getLedgerStream(String ledgerId,DateTime? since,);
+
+  /// Mengambil riwayat perubahan untuk ledger tertentu dalam rentang waktu yang ditentukan.
+  Future<List<RecordChange>> fetchHistory(
+    String ledgerId, 
+    DateTime start, 
+    DateTime end
+  );
 }
